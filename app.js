e7f455f2cf03abccd129a5444b3a74e4dc18970f
Srinu B <b.srinu462@gmail.com>
2024-10-25 13:50:38 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ * <div id="parent">
+ *  <div id="child"> 
+ *      <h1></h1>    
+ *  </div>
+ * </div>
+ * 
+ * 
+ */
+const parent = React.createElement("div", {id: "parent"}, 
+    React.createElement("div", {id: "child"},[ (React.createElement("h1", {}, "I am H1 Tag!!!!" )),(React.createElement("h2", {}, "I am H2 Tag!!!!" ))]))
+
+console.log(parent)
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(parent)
+   

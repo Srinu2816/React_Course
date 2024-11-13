2bc558d2dbd224169d9c13bc7c6f1b60543929c9
Srinu B <b.srinu462@gmail.com>
2024-11-13 12:23:39 +0530

    add the css

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
  * 
  */
 const parent = React.createElement("div", {id: "parent"}, 
-    React.createElement("div", {id: "child"},[ (React.createElement("h1", {}, "I am H1 Tag!!!!" )),(React.createElement("h2", {}, "I am H2 Tag!!!!" ))]))
+    React.createElement("div", {id: "child"},[ (React.createElement("h1", { id: "heading"}, "I am H1 Tag!!!!" )),(React.createElement("h2", {id: "heading"}, "I am H2 Tag!!!!" ))]))
 
 console.log(parent)
 const root = ReactDOM.createRoot(document.getElementById("root"))

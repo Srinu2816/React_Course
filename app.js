f11ebe93daccbd95a53f7d0c1a3fca4719ac0c35
Srinu B <b.srinu462@gmail.com>
2024-11-18 11:44:00 +0530

    second ep changes

--- app.js
+++ app.js
@@ -8,8 +8,11 @@
  * 
  * 
  */
+import React from "react";
+import ReactDOM from "react-dom/client"
+
 const parent = React.createElement("div", {id: "parent"}, 
-    React.createElement("div", {id: "child"},[ (React.createElement("h1", { id: "heading"}, "I am H1 Tag!!!!" )),(React.createElement("h2", {id: "heading"}, "I am H2 Tag!!!!" ))]))
+    React.createElement("div", {id: "child"},[ (React.createElement("h1", { id: "heading"}, "This is from Srinu....." )),(React.createElement("h2", {id: "heading"}, "I am H2 Tag!!!!" ))]))
 
 console.log(parent)
 const root = ReactDOM.createRoot(document.getElementById("root"))
